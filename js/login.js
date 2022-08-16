b82acf15874d99b434a0a5a88e2679f65b100443
yaoming16 <pabloperezzz16@gmail.com>
2022-08-16 19:17:49 -0300

    Mejoras a la entrega 1

--- js/login.js
+++ js/login.js
@@ -7,14 +7,27 @@ const form = document.getElementsByClassName("form");
  submit.addEventListener("click", function() {
 
      if (email.value.length !== 0  && password.value.length !== 0) {
+
         form.onsubmit = "return true";
         window.location.href = "portada.html" ;
-     } else if (email.value.length === 0) {
-        email.className = "alert-input";
+
+     } 
+     if (email.value.length === 0) {
+        email.classList = "alert-input";
         alert_p[0].innerHTML = "Ingresa tu e-mail "
-     } else if (password.value.length === 0) {
-        password.className = "alert-input";
+        
+     } else {
+      email.classList.remove("alert-input");
+      alert_p[0].innerHTML = ""
+   }
+     
+     if (password.value.length === 0) {
+        password.classList = "alert-input";
         alert_p[1].innerHTML = "Ingresa tu constraseña "
+        
+     } else {
+        password.classList.remove("alert-input");
+        alert_p[1].innerHTML = ""
      }
 
  })
