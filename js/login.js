b5a33b6f2bf8faa6d34c7d1d3acdfb8deea584ad
yaoming16 <pabloperezzz16@gmail.com>
2022-08-17 19:19:37 -0300

    Try 3

--- js/login.js
+++ js/login.js
@@ -33,6 +33,10 @@ const form = document.getElementsByClassName("form");
  })
 
 
+function redirect() {
+   alert("asd");
+}
+
 //  function onSignIn(googleUser) {
 //    var profile = googleUser.getBasicProfile();
 //    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
