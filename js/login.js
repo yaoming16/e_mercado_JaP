361f2a7daed0a17af1d0aa0284281c4991385739
yaoming16 <pabloperezzz16@gmail.com>
2022-08-20 12:59:00 -0300

    estilo botón de google

--- js/login.js
+++ js/login.js
@@ -72,7 +72,7 @@ window.onload = function () {
 
    google.accounts.id.renderButton(
          document.getElementById("google-button"),
-         { theme: "filled_blue", size: "medium", width: '200'}  // customization attributes
+         { theme: "filled_blue", size: "medium"}  // customization attributes
    );
    // also display the One Tap dialog on right side
    // important for auto login
